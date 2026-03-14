842240807fa129c3a2e5118d785210d6536865e6
Theo Beers <theo.beers@fu-berlin.de>
2026-03-14 13:31:54 -0400

    update analysis script

--- analyze.py
+++ analyze.py
@@ -5,8 +5,18 @@ def main():
     data = np.genfromtxt("results.csv", delimiter=",", names=True, dtype=int)
 
     for length in np.unique(data["length"]):
-        avg_guesses = int(np.rint(np.mean(data["guesses"][data["length"] == length])))
-        print(f"Length {length}: {avg_guesses}")
+        samples = data[data["length"] == length]
+        guesses = samples["guesses"]
+
+        avg_guesses = int(np.rint(np.mean(guesses)))
+        lucky_idx = int(np.argmin(guesses))
+        unlucky_idx = int(np.argmax(guesses))
+
+        print(f"Length {length}:")
+        print(f"  Samples: {samples.size}")
+        print(f"  Average guesses: {avg_guesses}")
+        print(f"  Luckiest run: {int(guesses[lucky_idx])} guesses")
+        print(f"  Unluckiest run: {int(guesses[unlucky_idx])} guesses")
 
 
 if __name__ == "__main__":
