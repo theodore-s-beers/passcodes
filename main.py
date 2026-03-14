8f9a132115dba334fe5a01cdc54acc6cfd230038
Theo Beers <theo.beers@fu-berlin.de>
2026-03-14 12:41:16 -0400

    update main script to generate samples

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+# We'll be saving experiment results in a CSV (Comma-Separated Values) file
+import csv
+
 # Import NumPy, a package that will make the math we do here much, much faster
 import numpy as np
 
 # Set up a random number generator for NumPy
 rng = np.random.default_rng()
+
+# The file name we'll use to save results
+RESULTS_FILE = "results.csv"
 
 
 # Since we're using only digits for our "passwords," it's more efficient to think of
@@ -46,20 +52,61 @@ def guess_passcode(passcode: int, limit: int) -> int:
     raise ValueError("Passcode not found")
 
 
+# This function combines `generate_passcode` and `guess_passcode`. We just tell it what
+# passcode length to use, and it generates a random passcode of that length, then
+# brute-force guesses it, and reports how many guesses it took. We'll run this function
+# many times for each passcode length, to generate many experimental samples.
+def generate_sample(length: int) -> dict[str, str | int]:
+    # Generate a random passcode of the given length
+    # This also calculates the largest number value that can fit in that many digits
+    passcode, limit = generate_passcode(length)
+    # print(f"Generated {length}-digit passcode: {passcode:0{length}d}")
+
+    # Brute-force guess the passcode, counting how many guesses it takes
+    guesses = guess_passcode(passcode, limit)
+    # print(f"Guesses to brute-force:     {guesses}")
+
+    # Return the results of this sample run
+    return {
+        "length": length,
+        "passcode": f"{passcode:0{length}d}",
+        "guesses": guesses,
+    }
+
+
+# This function takes a sample result and saves it to our CSV file
+def save_result(result: dict[str, str | int]) -> None:
+    # Open the CSV file to add the newest result to it
+    with open(RESULTS_FILE, mode="a", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=result.keys())
+
+        # If this is the first time we write to the file, we need to add a "header row"
+        if file.tell() == 0:
+            writer.writeheader()
+
+        # Add the result row to the CSV file
+        writer.writerow(result)
+
+
 # The actual "main" function that runs when we execute the script
 def main():
-    # Set the length and make sure it's valid
-    length = 8
-    assert length > 0, "Length must be a positive integer"
-    assert length <= 8, "Maximum length is 8 digits"
+    # Set the minimum and maximum passcode lengths we want to test
+    min_length = 4
+    max_length = 8
 
-    # Generate a random passcode
-    passcode, limit = generate_passcode(length)
-    print(f"Generated {length}-digit passcode: {passcode:0{length}d}")
+    # Make sure the lengths we're using are valid and feasible
+    assert min_length > 0, "Passcode lengths must be positive numbers"
+    assert min_length <= max_length, "Minimum length must be <= maximum length"
+    assert max_length <= 8, "Maximum practical passcode length is 8 digits"
 
-    # Brute-force the passcode and report how many guesses it took
-    guesses = guess_passcode(passcode, limit)
-    print(f"Guesses to brute-force:     {guesses}")
+    # How many samples to collect for each passcode length?
+    samples_per_length = 100
+
+    # Generate and save the desired number of samples for each passcode length
+    for length in range(min_length, max_length + 1):
+        for _ in range(samples_per_length):
+            result = generate_sample(length)
+            save_result(result)
 
 
 # This tells Python to run the main function only when we execute the script directly
